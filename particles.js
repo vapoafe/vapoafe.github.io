426e939ab0c0b55028666bc394e9fd6ea31d19e5
Vasileios Apostolidis-Afentoulis <13110598+vapoafe@users.noreply.github.com>
2025-02-01 14:56:40 +0200

    Update particles.js

--- particles.js
+++ particles.js
@@ -1,7 +1,7 @@
 particlesJS("particles-js", {
   particles: {
     number: { value: 150, density: { enable: true, value_area: 800 } },
-    color: { value: "#ffffff" },
+    color: { value: "#3e0070" },
     shape: {
       type: "circle",
       stroke: { width: 0, color: "#000000" },
@@ -21,7 +21,7 @@ particlesJS("particles-js", {
     line_linked: {
       enable: true,
       distance: 150,
-      color: "#ffffff",
+      color: "#3e0070",
       opacity: 0.4,
       width: 1
     },
